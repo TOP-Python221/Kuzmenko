568c1e564c179563870aee03c3438975e7d1a186
Dmitry <121047450+Dmitr4@users.noreply.github.com>
2023-02-02 16:27:13 +0500

    Homework

---
+++ 01.25/HW_01.25_1.sql
@@ -0,0 +1,29 @@
+select *
+  from `curators`;
+
+select *
+  from `departments`;
+
+select *
+  from `faculties`;
+
+select *
+  from `groups`;
+
+select *
+  from `groups_curators`;
+
+select *
+  from `groups_lectures`;
+
+select *
+  from `groups_students`;
+
+select *
+  from `lectures`;
+
+select *
+  from `students`;
+
+select *
+  from `teachers`;
