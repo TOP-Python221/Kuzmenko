363c4eb7794d36c667bd7afbd822638f405ec232
Dmitry <121047450+Dmitr4@users.noreply.github.com>
2023-02-02 16:36:56 +0500

    Update HW_01.25_2.sql

--- 01.25/HW_01.25_2.sql
+++ 01.25/HW_01.25_2.sql
@@ -2,7 +2,7 @@
 
 select t.`name`,
        t.`surname`,
-	     g.`name`
+       g.`name`
   from `teachers` as t
   join `lectures` as l
     on t.`id` = l.`teacher_id`
@@ -14,7 +14,7 @@ select t.`name`,
 -- 2 --
 
   select f.`name` as 'Факультеты',
-	       f.`financing` as 'Финансирование'
+         f.`financing` as 'Финансирование'
     from `faculties` as f
     join `departments` as d
       on f.`id` = d.`faculty_id`
@@ -24,9 +24,9 @@ group by `Финансирование`, `Факультеты`
 -- 3 --
 
   select `curators`.`surname` as 'Кураторы',
-		     group_concat(`groups`.`name` separator ', ') as 'Группы'
+         group_concat(`groups`.`name` separator ', ') as 'Группы'
     from `curators`
-	  join `groups_curators`
+	 join `groups_curators`
       on `curators`.`id` = `groups_curators`.`curator_id`
     join `groups`
       on `groups_curators`.`group_id` = `groups`.`id`
@@ -35,7 +35,7 @@ group by `Кураторы`;
 -- 4 --
 
 select c.`name`,
-	     c.`surname`
+       c.`surname`
   from `groups` as g
   join `groups_curators` as gc
     on g.`id` = gc.`group_id`
@@ -46,7 +46,7 @@ select c.`name`,
 -- 5 --
 
   select concat(c.`surname`, ' ',  c.`name`) as 'Полное имя',
-	       group_concat(f.`name` separator ', ') as 'Факультеты'
+         group_concat(f.`name` separator ', ') as 'Факультеты'
     from `curators` as c
     join `groups_curators` as gc
       on c.`id`= gc.`curator_id`
@@ -70,11 +70,11 @@ group by `Каферы`;
 -- 7 --
 
   select s.`name` as 'group'
-	  from `teachers` as t
-	  join `lectures` as l
-	    on t.`id` = l.`teacher_id`
-	  join `subjects` as s
-	    on s.`id`= l.`subject_id`
+    from `teachers` as t
+    join `lectures` as l
+      on t.`id` = l.`teacher_id`
+    join `subjects` as s
+      on s.`id`= l.`subject_id`
    where t.`name` = 'Doris' and  t.`surname` = 'Bentley'
 group by `group`;
 
@@ -83,20 +83,20 @@ group by `group`;
   select d.`name` as 'Кафедры'
     from `departments` as d
     join `groups` as g
-	    on d.`id` = g.`department_id`
+      on d.`id` = g.`department_id`
     join `groups_lectures` as gl
-	    on gl.`lecture_id` = g.`id`
+      on gl.`lecture_id` = g.`id`
     join `lectures` as l
-	    on l.`id` = gl.`lecture_id`
+      on l.`id` = gl.`lecture_id`
     join `subjects` as s
-	    on s.`id` = l.`subject_id`
+      on s.`id` = l.`subject_id`
    where s.`name` = 'Cursus A Enim'
 group by `Кафедры`;
 
 -- 9 --
 
   select f.`name` as 'Факультет',
-	       group_concat(g.`name` separator ', ') as 'Группы'
+         group_concat(g.`name` separator ', ') as 'Группы'
     from `groups` as g
     join `departments` as d
       on g.`department_id` = d.`id`
@@ -108,10 +108,10 @@ group by `Факультет`;
 -- 10 --
 
   select f.`name` as 'Факультеты',
-		     group_concat(g.`name` separator ', ') as 'Группы'
+         group_concat(g.`name` separator ', ') as 'Группы'
     from `groups` as g
     join `departments` as d
-	    on g.`department_id` = d.`id`
+      on g.`department_id` = d.`id`
     join `faculties` as f
       on f.`id` = d.`faculty_id`
    where g.`year` = 5
@@ -120,7 +120,7 @@ group by `Факультеты`;
 -- 11 --
 
 select concat(t.`name`, ' ', t.`surname`) as 'Полное имя',
-	     g.`name` as 'Группа',
+       g.`name` as 'Группа',
        s.`name` as 'Дисциплины'
   from `teachers` as t
   join `lectures` as l
